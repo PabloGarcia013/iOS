7154eaf3bd08dcc641f2f003edf02003636f339e
CICE <cice@AULA03MAC11.local>
2015-11-18 22:03:20 +0100

    second App finished

--- App2_Objetos/App2_Objetos/ViewController.swift
+++ App2_Objetos/App2_Objetos/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - VARIABLES LOCALES
+    
+    
+    //MARK: - IB OUTLET
+    
+    
+    @IBOutlet weak var myName: UILabel!
+    @IBOutlet weak var myPhone: UILabel!
+    @IBOutlet weak var myAddres: UILabel!
+    
+    //MARK: - IB ACTION
+    @IBAction func myButton(sender: AnyObject) {
+        myName.text = "Pablo García";
+        myPhone.text = "61234567"
+        myAddres.text = "Calle falsa 1234"
+    }
+    
+    //MARK: - LIFE APP
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
 }
 
