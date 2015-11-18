fbc5e420c47c2e2774a89a3bec445ef3524be135
CICE <cice@AULA03MAC11.local>
2015-11-18 19:20:02 +0100

    first upload of the App number 2

---
+++ App2_Objetos/App2_Objetos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App2_Objetos
+//
+//  Created by CICE on 18/11/15.
+//  Copyright © 2015 CICE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
