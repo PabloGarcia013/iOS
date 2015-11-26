2dc283baf514f26f81ca7cef4e436d728d8c4d99
CICE <cice@AULA03MAC11.local>
2015-11-26 19:11:18 +0100

    modified before delete

--- CICE/App4_TabBar/App4_TabBar/ViewController.swift
+++ CICE/App4_TabBar/App4_TabBar/ViewController.swift
@@ -23,34 +23,34 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onClickSocial(sender: AnyObject) {
-        var tittle = "Redes Sociales"
-        var mutableArray : NSMutableArray = ["Facebook","Twitter","Tuenti","iCoSpartan"]
+        let tittle = "Redes Sociales"
+        let mutableArray : NSMutableArray = ["Facebook","Twitter","Tuenti","iCoSpartan"]
         openAlertController(mutableArray, _title: tittle)
     }
     
     @IBAction func onClickMessage(sender: AnyObject) {
-        var tittle = "Envio de Mensajes"
-        var mutableArray : NSMutableArray = ["Mail","SMS","Telegram","Whatsapp"]
+        let tittle = "Envio de Mensajes"
+        let mutableArray : NSMutableArray = ["Mail","SMS","Telegram","Whatsapp"]
         openAlertController(mutableArray, _title: tittle)
     }
 
     @IBAction func onClickCamera(sender: AnyObject) {
-        var tittle = "Escoge la Cámara"
-        var mutableArray : NSMutableArray = ["Desde la biblioteca","Abre la App Photo","Cámara","Photo Kit"]
+        let tittle = "Escoge la Cámara"
+        let mutableArray : NSMutableArray = ["Desde la biblioteca","Abre la App Photo","Cámara","Photo Kit"]
         openAlertController(mutableArray, _title: tittle)
     }
     
     @IBAction func onClickChat(sender: AnyObject) {
-        var tittle = "Escribe un Chat"
-        var mutableArray : NSMutableArray = ["desde Whatsapp","desde iCoSpartan","desde Twitter","desde Tuenti"]
+        let tittle = "Escribe un Chat"
+        let mutableArray : NSMutableArray = ["desde Whatsapp","desde iCoSpartan","desde Twitter","desde Tuenti"]
         openAlertController(mutableArray, _title: tittle)
     }
     
     func openAlertController(_fields: NSMutableArray, _title: String){
-        var number_of_fields = _fields.count
-        let alertController = UIAlertController(title: _title, message: "", preferredStyle: UIAlertControllerStyle.Alert)
+        let number_of_fields = _fields.count
+        let alertController = UIAlertController(title: _title, message: "", preferredStyle: UIAlertControllerStyle.ActionSheet)
         for var index = 0; index < number_of_fields; ++index{
-            let alertAction = UIAlertAction(title: _fields[index] as! String, style: UIAlertActionStyle.Default, handler: nil)
+            let alertAction = UIAlertAction(title: _fields[index] as? String, style: UIAlertActionStyle.Default, handler: nil)
             alertController.addAction(alertAction)
         }
         let okAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
